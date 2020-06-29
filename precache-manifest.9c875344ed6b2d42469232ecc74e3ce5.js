31efa8ee05997562626e8a01bac68d0a0e20bd19
Ben Hansen <bnhansn@gmail.com>
2020-06-28 17:33:35 -0700

    Updates

---
+++ precache-manifest.9c875344ed6b2d42469232ecc74e3ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e3c2ff55cdfa6daff62fbe348a538f",
+    "url": "/fw-widget-demo/index.html"
+  },
+  {
+    "revision": "1d1c6b1a8a3dd621428f",
+    "url": "/fw-widget-demo/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "2a494dacdf89b972f5bd",
+    "url": "/fw-widget-demo/static/js/2.fdb235cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fw-widget-demo/static/js/2.fdb235cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1c6b1a8a3dd621428f",
+    "url": "/fw-widget-demo/static/js/main.b13dbf58.chunk.js"
+  },
+  {
+    "revision": "b9c2143191e2122152c7",
+    "url": "/fw-widget-demo/static/js/runtime-main.31756a0b.js"
+  }
+]);
